c805aa4c14c755f63cf7511bd86ababcb7861406
Krzysztof Domino <kdomino@iitis.pl>
2024-10-23 14:58:38 +0200

    jobs and machines

--- JobShop_QUBO/jobshop.py
+++ JobShop_QUBO/jobshop.py
@@ -1,9 +1,28 @@
-
+from itertools import chain
 
 class Job:
     """ stores jobs """
-    def __init__(self):
-        self.schedule = {}
-        self.release = 0
-        self.due = 0
-        self.weight = 0
+    def __init__(self, schedule:dict, release:int, due:int, weight:float):
+        """ schedule should be initialised {m1: p(j, m1), ....} """
+        self.schedule = schedule
+        self.release = release
+        self.due = due
+        self.weight = weight
+
+
+
+class JobShop:
+    """ stores job shop problem """
+
+    def get_no_machines(self, jobs):
+        machines = []
+        for job in jobs:
+            for m in job.schedule.keys():
+                if m not in machines:
+                    machines.append(m)
+        return len(machines)
+
+    def __init__(self, jobs: dict):
+        self.jobs = jobs
+        self.no_jobs = len(jobs)
+        self.no_machines = self.get_no_machines(jobs)
--- tests/test_jobshop.py
+++ tests/test_jobshop.py
@@ -1,11 +1,25 @@
-from JobShop_QUBO import Job
+from JobShop_QUBO import Job, JobShop
 
 
-def test_Job():
-    "test empty jon"
-    J = Job()
-    assert J.schedule == {}
-    assert J.release == 0
-    assert J.due == 0
-    assert J.weight == 0
-    print("tests")
+def test_JobShop():
+    "test job"
+    schedule = {1:2, 2:3}
+    J = Job(schedule=schedule, release=1, due=10, weight=0.5)
+    assert J.schedule == {1:2, 2:3}
+    assert J.release == 1
+    assert J.due == 10
+    assert J.weight == 0.5
+
+    "test job shop"
+    JS = JobShop([J, J])
+    assert JS.jobs == [J,J]
+    assert JS.no_jobs == 2
+    assert JS.no_machines == 2
+
+    J1 = Job(schedule={2:2, 3:2}, release=1, due=10, weight=0.5)
+    J2 = Job(schedule={1:1, 3:3}, release=1, due=10, weight=0.5)
+
+    JS = JobShop([J, J1, J2])
+    assert JS.jobs == [J,J1, J2]
+    assert JS.no_jobs == 3
+    assert JS.no_machines == 3
