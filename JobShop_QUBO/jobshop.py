2cfd312550d17cf8e4e0273b1711463add0f14d5
Krzysztof Domino <kdomino@iitis.pl>
2024-10-24 09:01:27 +0200

    encoded initial input of jobs and machines

--- JobShop_QUBO/jobshop.py
+++ JobShop_QUBO/jobshop.py
@@ -2,12 +2,23 @@ from itertools import chain
 
 class Job:
     """ stores jobs """
-    def __init__(self, schedule:dict, release:int, due:int, weight:float):
+
+    def get_machines_ids(self):
+        return list(self.schedule.keys())
+
+
+    def __init__(self, id:int, schedule:dict, release:int, due:int, weight:float):
         """ schedule should be initialised {m1: p(j, m1), ....} """
+
+
+        self.id =id
         self.schedule = schedule
         self.release = release
         self.due = due
         self.weight = weight
+
+
+
 
 
 
@@ -17,12 +28,22 @@ class JobShop:
     def get_no_machines(self, jobs):
         machines = []
         for job in jobs:
-            for m in job.schedule.keys():
-                if m not in machines:
-                    machines.append(m)
-        return len(machines)
+            machines += job.get_machines_ids()
+    
+        unique_m = list(set(machines))
+        return len(unique_m)
 
-    def __init__(self, jobs: dict):
+
+    def get_ids(self, jobs:list):
+        return [job.id for job in jobs]
+
+
+    def __init__(self, jobs:list):
+
+        ids = self.get_ids(jobs)
+        if not len(ids) == len(list(set(ids))):
+            raise ValueError ("jobs ids not unique")
+
         self.jobs = jobs
         self.no_jobs = len(jobs)
         self.no_machines = self.get_no_machines(jobs)
--- tests/test_jobshop.py
+++ tests/test_jobshop.py
@@ -1,25 +1,44 @@
+import pytest
 from JobShop_QUBO import Job, JobShop
 
 
-def test_JobShop():
+def test_Job():
     "test job"
     schedule = {1:2, 2:3}
-    J = Job(schedule=schedule, release=1, due=10, weight=0.5)
+    J = Job(id = 1, schedule=schedule, release=1, due=10, weight=0.5)
+    assert J.id == 1
     assert J.schedule == {1:2, 2:3}
     assert J.release == 1
     assert J.due == 10
     assert J.weight == 0.5
 
+    assert J.get_machines_ids() == [1,2]
+
+
+
+
+def test_JobShop():
     "test job shop"
-    JS = JobShop([J, J])
-    assert JS.jobs == [J,J]
-    assert JS.no_jobs == 2
+
+    J = Job(id = 1, schedule= {1:2, 2:3}, release=1, due=10, weight=0.5)
+    JS = JobShop([J])
+    assert JS.jobs == [J]
+    assert JS.no_jobs == 1
     assert JS.no_machines == 2
 
-    J1 = Job(schedule={2:2, 3:2}, release=1, due=10, weight=0.5)
-    J2 = Job(schedule={1:1, 3:3}, release=1, due=10, weight=0.5)
+    J1 = Job(id = 2, schedule={2:2, 3:2}, release=1, due=10, weight=0.5)
+    J2 = Job(id = 3, schedule={1:1, 3:3}, release=1, due=10, weight=0.5)
 
     JS = JobShop([J, J1, J2])
     assert JS.jobs == [J,J1, J2]
     assert JS.no_jobs == 3
     assert JS.no_machines == 3
+
+
+
+def test_JobShop_errors():
+    J = Job(id = 1, schedule={1:2, 2:3}, release=1, due=10, weight=0.5)
+    with pytest.raises(ValueError) as exc_info:
+        S = JobShop([J, J])
+    assert exc_info.value.args[0] == "jobs ids not unique"
+    
